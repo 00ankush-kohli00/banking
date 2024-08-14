7824cacb908bdef06083fdb012aa077b96e42d03
00ankush-kohli00 <ankushkohli872@gmail.com>
2024-08-14 20:59:49 +0530

    implement bankCard

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,31 @@
+import Image from "next/image";
+import Sidebar from "../../components/Sidebar";
+import MobileNav from "../../components/MobileNav";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: "Ankush", lastName: "Kohli" };
+
   return (
-    <main>
-      SIDEBAR
-      {children}
+    <main className="h-screen w-full flex font-inter">
+      <Sidebar user={loggedIn} />
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image
+            src={"/icons/logo.svg"}
+            width={30}
+            height={30}
+            alt="menu icon"
+          />
+          <div>
+            <MobileNav user={loggedIn} />
+          </div>
+        </div>
+        {children}
+      </div>
     </main>
   );
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import HeaderBox from "../../components/HeaderBox";
+import TotalBalaceBox from "../../components/TotalBalaceBox";
+import RightSidebar from "../../components/RightSidebar";
 
 const Home = () => {
+  const loggedIn = {
+    firstName: "Ankush",
+    lastName: "Kohli",
+    email: "ankushkohli873@gmail.com",
+  };
   return (
-    <div>Home</div>
-  )
-}
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transaction efficiently."
+          />
+          <TotalBalaceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+        RECENT TRANSACTION
+      </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.5 }, { currentBalance: 1243.9 }]}
+      />
+    </section>
+  );
+};
 
-export default Home
+export default Home;
